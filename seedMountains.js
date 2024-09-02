e59f2af14ce549442ee56f40324fa61a1f736dd6
FX Wood <fxwood.work@gmail.com>
2024-09-02 15:22:04 -0700

    fix env vars, formatting

--- seedMountains.js
+++ seedMountains.js
@@ -1,58 +1,60 @@
-const Mountain = require('./models/mountain')
-const mongoose = require('mongoose')
+const Mountain = require("./models/mountain");
+const mongoose = require("mongoose");
 const mountains = [
-    {
-        name: 'Crystal Mountain',
-        website: 'www.crystalmountainresort.com',
-        twitter: 'https://twitter.com/crystalmt?lang=en',
-        weather: 'https://www.crystalmountainresort.com/the-mountain/mountain-report/weather-conditions/',
-        sizzle: 'The Largest ski resort in Washington State with 2,600 acres and over 50 named runs. Skiers and Snowboarders from all around Washington, the Pacific Northwest and the world flock to Crystal Mountain for the incredible terrain, breathtaking views of Mt. Rainier and Washington’s only Gondola.'
-    },
-    {
-        name: 'Stevens Pass',
-        website: '',
-        twitter: '',
-        weather: '',
-        sizzle: 'Skiing at Stevens Pass is truly a unique experience; a great way to wind down after work, or just get your turns in without worrying about crowds',
-    },
-    {
-        name: 'The Summit at Snoqualmie',
-        website: '',
-        twitter: '',
-        weather: '',
-        sizzle: "Seattle's Home Mountain! The Summit has four unique areas and the most night skiing in Washington, plus snow tubing and Nordic skiing. All just an hour from the city on I-90"
-    }
-]
+  {
+    name: "Crystal Mountain",
+    website: "www.crystalmountainresort.com",
+    twitter: "https://twitter.com/crystalmt?lang=en",
+    weather:
+      "https://www.crystalmountainresort.com/the-mountain/mountain-report/weather-conditions/",
+    sizzle:
+      "The Largest ski resort in Washington State with 2,600 acres and over 50 named runs. Skiers and Snowboarders from all around Washington, the Pacific Northwest and the world flock to Crystal Mountain for the incredible terrain, breathtaking views of Mt. Rainier and Washington’s only Gondola.",
+  },
+  {
+    name: "Stevens Pass",
+    website: "",
+    twitter: "",
+    weather: "",
+    sizzle:
+      "Skiing at Stevens Pass is truly a unique experience; a great way to wind down after work, or just get your turns in without worrying about crowds",
+  },
+  {
+    name: "The Summit at Snoqualmie",
+    website: "",
+    twitter: "",
+    weather: "",
+    sizzle:
+      "Seattle's Home Mountain! The Summit has four unique areas and the most night skiing in Washington, plus snow tubing and Nordic skiing. All just an hour from the city on I-90",
+  },
+];
 
-if (process.env.NODE_ENV == 'production') {
-    mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
+if (process.env.NODE_ENV == "production") {
+  mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
 } else {
-    mongoose.connect('mongodb://localhost/project4', {useNewUrlParser: true});
-} 
+  mongoose.connect("mongodb://localhost/project4", { useNewUrlParser: true });
+}
 
 const db = mongoose.connection;
 
-db.on('open', () => {
-    console.log(`Connected to Mongo on ${db.host}: ${db.port}`)
-})
-db.on('error', (err) => {
-    console.log(`Database error:\n${err}`)
-})
-
+db.on("open", () => {
+  console.log(`Connected to Mongo on ${db.host}: ${db.port}`);
+});
+db.on("error", (err) => {
+  console.log(`Database error:\n${err}`);
+});
 
 async function makeMtns(mtns) {
-    console.log('starting')
-    for (mtn of mtns) {
-        try {
-            let raw = new Mountain(mtn)
-            let saved = await raw.save()
-            console.log('saved', saved)
-        } catch(err) {
-            console.log('error', err)
-        }
+  console.log("starting");
+  for (mtn of mtns) {
+    try {
+      let raw = new Mountain(mtn);
+      let saved = await raw.save();
+      console.log("saved", saved);
+    } catch (err) {
+      console.log("error", err);
     }
-    db.close()
+  }
+  db.close();
 }
 
-makeMtns(mountains)
-
+makeMtns(mountains);
--- server.js
+++ server.js
@@ -1,59 +1,86 @@
-require('dotenv').config();
-const express = require('express');
-const mongoose = require('mongoose');
-const expressJWT = require('express-jwt');
-const RateLimit = require('express-rate-limit');
+require("dotenv").config();
+const express = require("express");
+const mongoose = require("mongoose");
+const expressJWT = require("express-jwt");
+const RateLimit = require("express-rate-limit");
 
 const app = express();
-app.use(express.urlencoded({extended: false}));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
 const loginLimiter = new RateLimit({
-    windowMs: 5 * 60 * 1000, // 5 minutes
-    max: 3, 
-    delayMs: 0, // disabled
-    message: JSON.stringify({type: 'error', message: "Maximum login attempts exceeded!" })
-})
+  windowMs: 5 * 60 * 1000, // 5 minutes
+  max: 3,
+  delayMs: 0, // disabled
+  message: JSON.stringify({
+    type: "error",
+    message: "Maximum login attempts exceeded!",
+  }),
+});
 
 const signupLimiter = new RateLimit({
-    windowMs: 60 * 60 * 1000,
-    max: 3,
-    delayMs: 0, // disabled
-    message: JSON.stringify({type: 'error', message: 'Account creation maximum exceeded!' })
-})
+  windowMs: 60 * 60 * 1000,
+  max: 3,
+  delayMs: 0, // disabled
+  message: JSON.stringify({
+    type: "error",
+    message: "Account creation maximum exceeded!",
+  }),
+});
 
-if (process.env.NODE_ENV == 'production') {
-    app.use(express.static(__dirname + '/client/build'));
-    mongoose.connect(process.env.MONGODB_URI, {useMongoClient: true});
+if (process.env.NODE_ENV == "production") {
+  app.use(express.static(__dirname + "/client/build"));
+  mongoose.connect(process.env.MONGODB_URI, { useMongoClient: true });
 } else {
-    mongoose.connect('mongodb://localhost/project4', {useNewUrlParser: true});
-} 
+  mongoose.connect("mongodb://localhost/project4", { useNewUrlParser: true });
+}
 
 const db = mongoose.connection;
 
-db.on('open', () => {
-    console.log(`Connected to Mongo on ${db.host}: ${db.port}`)
-})
-db.on('error', (err) => {
-    console.log(`Database error:\n${err}`)
-})
+db.on("open", () => {
+  console.log(`Connected to Mongo on ${db.host}: ${db.port}`);
+});
+db.on("error", (err) => {
+  console.log(`Database error:\n${err}`);
+});
 
+app.use("/api/auth/login", loginLimiter);
+app.use("/api/auth/signup", signupLimiter);
 
-app.use('/api/auth/login', loginLimiter);
-app.use('/api/auth/signup', signupLimiter);
+app.use("/api/auth", require("./routes/auth"));
+app.use(
+  "/api/user",
+  expressJWT({ secret: process.env.JWT_SECRET }),
+  require("./routes/user"),
+);
+app.use(
+  "/api/ride",
+  expressJWT({ secret: process.env.JWT_SECRET }),
+  require("./routes/ride"),
+);
+app.use(
+  "/api/mountains",
+  expressJWT({ secret: process.env.JWT_SECRET }),
+  require("./routes/mountain"),
+);
+app.use(
+  "/api/share",
+  expressJWT({ secret: process.env.JWT_SECRET }),
+  require("./routes/share"),
+);
 
-app.use('/api/auth', require('./routes/auth'))
-app.use('/api/user', expressJWT({ secret: process.env.JWT_SECRET }), require('./routes/user'))
-app.use('/api/ride', expressJWT({ secret: process.env.JWT_SECRET }), require('./routes/ride'))
-app.use('/api/mountains', expressJWT({ secret: process.env.JWT_SECRET }), require('./routes/mountain'))
-app.use('/api/share', expressJWT({ secret: process.env.JWT_SECRET }), require('./routes/share'))
+app.get("*", (err, res) => {
+  console.log("GET *");
+  res.send(__dirname + "/client/build/index.html");
+});
 
-app.get('*', (err, res) => {
-    console.log('GET *')
-    res.send(__dirname + '/client/build/index.html')
-})
-app.listen(process.env.PORT, () => {
-    console.log(`You're listening to the sweet sounds of ${process.env.PORT} project4 in the morning...`)
-    console.log(`Oh, and the port is`, process.env.PORT)
-    console.log(`Node environment: ${process.env.NODE_ENV}`)
-})
+const port = process.env.PORT || 3001;
+const nodeEnv = process.env.NODE_ENV || "development";
+app.listen(port, () => {
+  console.log(
+    `You're listening to the sweet sounds of ${port} mountainroad in the morning...`,
+  );
+  console.log(`Oh, and the port is`, port);
+  console.log(`Node environment: ${nodeEnv}`);
+});
+
